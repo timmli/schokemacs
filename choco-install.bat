c8a9d513f4ef76c1da0393fcc9c15e6f0fb2e551
timmli <lichte@phil.hhu.de>
2018-10-13 18:45:27 +0200

    renamed files

--- choco-install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem Execute with administrator rights!
-rem Taken from https://chocolatey.org/install
-
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
